eb543c5adee33fc196ef62354010600c7f18a531
사명구(Sa Myeonggu) <quest4i@me.com>
2016-08-29 17:41:07 +0900

    h2 database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO customers(first_name, last_name) VALUES ('Nobita', '노비');
+INSERT INTO customers(first_name, last_name) VALUES ('Takeshi', 'Goda');
+INSERT INTO customers(first_name, last_name) VALUES ('Suneo', 'Honekawa');
+INSERT INTO customers(first_name, last_name) VALUES ('Shizuka', 'Minamoto');
